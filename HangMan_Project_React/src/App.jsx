570e070fa6224d6aac6632d07e611d38a67018a1
Ankush2107 <ankush.kumar210701@gmail.com>
2024-11-02 16:46:03 +0530

    reuseable component

--- HangMan_Project_React/src/App.jsx
+++ HangMan_Project_React/src/App.jsx
@@ -1,7 +1,13 @@
+import TextInput from "./components/TextInput/TextInput"
+
 function App() {
   return (
     <div>
-      
+      <TextInput 
+        label={"Enter some text"}
+        placeholder={"Enter some text here..."}
+        onChangeHandler={(e) => console.log(e.target.value)}
+      />
     </div>
   )
 }
