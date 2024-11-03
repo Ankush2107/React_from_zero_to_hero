82b5317f4aae22b869bda5c3544c4da95d26cf58
Ankush2107 <ankush.kumar210701@gmail.com>
2024-11-03 15:14:03 +0530

    using useSearchParams for getting data from one page to another

--- HangMan_Project_React/src/pages/PlayGame.jsx
+++ HangMan_Project_React/src/pages/PlayGame.jsx
@@ -1,7 +1,14 @@
+import { Link, useSearchParams } from "react-router-dom";
+
 function PlayGame() {
+
+    let [searchParams] = useSearchParams();
+    console.log(searchParams.get("text"))
+
     return (
         <>
             <h1>Play Game</h1>
+            <Link to="/start" className="text-blue-500">Start Game Link</Link>
         </>
     )
 }
